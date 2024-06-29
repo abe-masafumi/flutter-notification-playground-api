58697bb65922f13bb32639d95b230863011e9fb0
masafumi <masa455.wv@gmail.com>
2024-06-30 03:12:28 +0900

    modelsの作成からviewsの作成まで

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import MaleUser
+from .serializers import MaleUserSerializer
+
+# ルート画面
+def home_view(request):
+    return render(request, 'home.html')
 
 # Create your views here.
+class MyModelViewSet(viewsets.ModelViewSet):
+    queryset = MaleUser.objects.all()
+    serializer_class = MaleUserSerializer
