12605e16b7c0d8abcf2702f84bbe3e5f191c5959
masafumi <masa455.wv@gmail.com>
2024-06-30 01:30:14 +0900

    アプリケーションの作成

---
+++ myapp/models.py
@@ -0,0 +1,57 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(models.Model):
+    ROLES = (
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+    )
+    firebase_uid = models.CharField(max_length=255, unique=True)
+    role = models.CharField(max_length=10, choices=ROLES)
+
+class MaleUser(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    username = models.CharField(max_length=100)
+    birthday = models.DateField()
+    address = models.CharField(max_length=255)
+    introduction = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class FemaleUser(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    username = models.CharField(max_length=100)
+    birthday = models.DateField()
+    introduction = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username
+
+
+class ProfileImage(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    image_url = models.URLField()
+
+    def __str__(self):
+        return self.image_url
+
+
+class Hobby(models.Model):
+    type = models.CharField(max_length=100)
+    male_user = models.ForeignKey(MaleUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.type
+
+
+class ApiLog(models.Model):
+    endpoint = models.CharField(max_length=255)
+    request_time = models.DateTimeField(auto_now_add=True)
+    method = models.CharField(max_length=10)
+    status_code = models.IntegerField()
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.endpoint} - {self.method}"
---
+++ myapp/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import MaleUser, FemaleUser, ProfileImage, Hobby, ApiLog, CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'firebase_uid', 'role']
+
+class MaleUserSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
+
+    class Meta:
+        model = MaleUser
+        fields = '__all__'
+
+class FemaleUserSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
+
+    class Meta:
+        model = FemaleUser
+        fields = '__all__'
+
+class ProfileImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProfileImage
+        fields = '__all__'
+
+class HobbySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hobby
+        fields = '__all__'
+
+class ApiLogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ApiLog
+        fields = '__all__'
