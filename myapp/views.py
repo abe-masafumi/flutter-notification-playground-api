0c5384f7f25266a59e58d273280572723a143624
masafumi <masa455.wv@gmail.com>
2024-06-30 18:10:39 +0900

    削除処理でレスポンスメッセージを返す

--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets, exceptions, status
+from rest_framework.response import Response
 from rest_framework import viewsets, exceptions
 from .models import MaleUser
 from .serializers import MaleUserCreateSerializer, MaleUserUpdateSerializer, MaleUserRetrieveSerializer
@@ -20,3 +22,9 @@ class MyModelViewSet(viewsets.ModelViewSet):
             return MaleUserRetrieveSerializer
         else:
             raise exceptions.MethodNotAllowed(self.action)
+
+# 削除処理でレスポンスデータを返す
+    def destroy(self, request, *args, **kwargs):
+            instance = self.get_object()
+            instance.delete()
+            return Response({'detail': 'Deletion was successful.'}, status=status.HTTP_204_NO_CONTENT)
