8abfcb353dcc9dbb621cc811e0da08a33586d61b
masafumi <masa455.wv@gmail.com>
2024-07-05 21:38:22 +0900

    必要なリクエストのみに是正

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, exceptions, mixins, status
+from rest_framework import viewsets, mixins, status
 from rest_framework.response import Response
 from .serializers import CustomUser, CustomUserCreateSerializer, MaleUser, MaleUserUpdateSerializer, MaleUserRetrieveSerializer, Hobby, HobbySerializer
 
@@ -8,17 +8,21 @@ def home_view(request):
     return render(request, 'home.html')
 
 # カスタムビューを基点に処理
-class CustomUserViewSet(viewsets.ModelViewSet):
+class CustomUserViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     queryset = CustomUser.objects.all()
+    serializer_class = CustomUserCreateSerializer
 
     def get_serializer_class(self):
         if self.action == 'create':
             return CustomUserCreateSerializer
-        else:
-            raise exceptions.MethodNotAllowed(self.action)
 
 # 男性ユーザー情報操作
-class MaleUserViewSet(viewsets.ModelViewSet):
+class MaleUserViewSet(
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    viewsets.GenericViewSet
+):
     queryset = MaleUser.objects.all()
 
     def get_serializer_class(self):
@@ -26,8 +30,12 @@ class MaleUserViewSet(viewsets.ModelViewSet):
             return MaleUserUpdateSerializer
         elif self.action == 'retrieve':
             return MaleUserRetrieveSerializer
-        else:
-            raise exceptions.MethodNotAllowed(self.action)
+        
+    # 削除処理でレスポンスデータを返す
+    def destroy(self, request, *args, **kwargs):
+            instance = self.get_object()
+            instance.delete()
+            return Response({'detail': 'Deletion was successful.'}, status=status.HTTP_204_NO_CONTENT)
 
 # 趣味の作成、削除
 class HobbyViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet):
